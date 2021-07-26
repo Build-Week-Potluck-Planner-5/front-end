b34be6afd701a1c79af385724b0f992c03653cb8
cn8817 <christinenguyen1433@gmail.com>
2021-07-26 12:45:25 -0700

    created test content component

---
+++ potluck/src/Components/Content.js
@@ -0,0 +1,37 @@
+/*element structure
+    <div>
+            <img/>
+        <div>
+            <h3>Content Title</h3>
+            <p>content</p>
+            <button>Learn More</button>
+        </div>
+    </div>
+*/
+
+const root = document.querySelector('#root')
+function createContent({title, content}) {
+
+    const contentDiv = document.createElement('div')
+    const contentImg = document.createElement('img')
+    const infoDiv = document.createElement('div')
+    const contentTitle = document.createElement('h3')
+    const contentInfo = document.createElement('p')
+    const contentButton = document.createElement('button')
+
+    contentDiv.appendChild(contentImg)
+    contentDiv.appendChild(infoDiv)
+    infoDiv.appendChild(contentTitle)
+    infoDiv.appendChild(contentInfo)
+    infoDiv.appendChild(contentButton)
+
+    contentTitle.textContent = title
+    contentInfo.textContent = content
+    contentButton.textContent = 'Learn More'
+
+    return contentDiv
+
+}
+
+const testContent = createContent({title: 'test title', content: 'test content'})
+root.appendChild(testContent)
