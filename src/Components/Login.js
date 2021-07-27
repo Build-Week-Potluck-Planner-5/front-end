00265e5204274c68fbf829b376a998b465908c62
JorshuaG <joshbgearheart@gmail.com>
2021-07-27 11:49:39 -1000

    newest login register update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,6 @@
-
-
-
-
 import React, { useState, useEffect } from "react";
-
+import * as yup from "yup";
+import { reach } from "yup";
 
 // username
 // password
@@ -11,38 +8,64 @@ import React, { useState, useEffect } from "react";
 // nice to have -- form validation and error handling messages
 
 function Login(props) {
+  const initialFormErrors = {
+    username: "",
+    password: "",
+  };
+  const [loginErr, setLoginErr] = useState(initialFormErrors);
+
   const [userData, setUserData] = useState({
     username: "",
     password: "",
   });
 
+  const loginSchema = yup.object().shape({
+    username: yup
+      .string()
+      .trim()
+      .required("Username is required")
+      .min(3, "Username must be at least 3 characters long"),
+    password: yup.string().trim().required("Must enter your password"),
+  });
+
+  const validate = (name, value) => {
+    reach(loginSchema, name)
+      .validate(value)
+      .then(() => setLoginErr({ ...loginErr, [name]: "" }))
+      .catch((err) => setLoginErr({ ...loginErr, [name]: err.errors[0] }));
+  };
+
   const handleChange = (evt) => {
-    return setUserData(...userData, { [evt.target.name]: evt.target.value });
+    const target = evt.target;
+    const value = target.value;
+    const name = target.name;
+    validate(name, value);
+    setUserData({ ...userData, [name]: value });
   };
   console.log(userData);
-  // const handleSubmit = (evt) => {
-  //   evt.preventDefault();
-  //   const { username, first_name, last_name, password } = userData;
-  //   axios
-  //     .post(
-  //       url,
-  //       {
-  //         user: {
-  //           username: username,
-  //           first_name: first_name,
-  //           last_name: last_name,
-  //           password: password,
+  //   const handleSubmit = (evt) => {
+  //     evt.preventDefault();
+  //     const { username, first_name, last_name, password } = userData;
+  //     axios
+  //       .post(
+  //         url,
+  //         {
+  //           user: {
+  //             username: username,
+  //
+  //
+  //             password: password,
+  //           },
   //         },
-  //       },
-  //       { withCredentials: true }
-  //     )
-  //     .then((resp) => {
-  //       return console.log("registration response", resp.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log("registration error", err);
-  //     });
-  // };
+  //         { withCredentials: true }
+  //       )
+  //       .then((resp) => {
+  //         return console.log("login response", resp.data);
+  //       })
+  //       .catch((err) => {
+  //         console.log("login error", err);
+  //       });
+  //   };
   return (
     <form>
       <div>
@@ -50,17 +73,27 @@ function Login(props) {
           onChange={handleChange}
           value={userData.username}
           placeholder="Username"
+          name="username"
           type="text"
+          required
         ></input>
-        <input placeholder="Password" type="text"></input>
+        <input
+          onChange={handleChange}
+          value={userData.password}
+          placeholder="Password"
+          name="password"
+          type="password"
+          required
+        ></input>
 
         <button>Submit</button>
+        <div>
+          <p>{loginErr.username}</p>
+          <p>{loginErr.password}</p>
+        </div>
       </div>
     </form>
   );
 }
 
 export default Login;
-
-
-
