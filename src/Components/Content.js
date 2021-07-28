365f0ee47da18c15b4b59910b21a8b396e5c8441
cn8817 <christinenguyen1433@gmail.com>
2021-07-27 20:59:37 -0700

    aligned nav bar with hero

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,17 @@
 // basic marketing content about application features
 // image plus text content
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledContent = styled.div`
+    background-color: #F4F4F4;
+    margin: 2% 5%;
+    padding: 2% 0;
+`
 
 function Content () {
     return(
+        <StyledContent>
         <div className='contentDiv'>
             <h3>What we have to offer</h3>
             <div className='infoDiv one'>
@@ -23,6 +31,7 @@ function Content () {
                 </ul>
             </div>
         </div>
+        </StyledContent>
     )
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,23 +4,48 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledHeader = styled.div`
+margin: 0 2em;
+
+nav{
+    display:flex;
+    justify-content: flex-end;
+    flex-direction: row;
+}
+
+.link{
+    padding:0 3em;
+}
+
+p{ 
+    color: black;
+}
+`
 
 function Header () {
     return (
+        <StyledHeader>
         <header>
             <nav>
-            <Link to='/'>
+            <Link to='/' style={{ textDecoration: 'none' }}>
                 <div>
                     <img src='' alt=''/>
                 </div>
             </Link>
-                <div>
-                    <Link to ='/'>Home</Link>
-                    <Link to ='/signup'>Sign Up</Link>
-                    <Link to ='/login'>Login</Link>
+                <div className='link'> 
+                    <Link to ='/' style={{ textDecoration: 'none' }}><p>Home</p></Link>
+                </div>
+                <div className='link'>
+                    <Link to ='/signup' style={{ textDecoration: 'none' }}><p>Sign Up</p></Link>
+                </div>
+                <div className='link'>
+                    <Link to ='/login' style={{ textDecoration: 'none' }}><p>Login</p></Link>
                 </div>
             </nav>
         </header>
+        </StyledHeader>
     )
 }
 
