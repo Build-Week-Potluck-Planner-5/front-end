3969aadfe50477a545a1757ed7b9ec568b35b9aa
cn8817 <christinenguyen1433@gmail.com>
2021-07-29 19:36:18 -0700

    fixed app.js route path to the correct component potluck invites

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
       <Route path="/mypotlucks">
           <MyPotlucks />
       </Route>
-      <Route path="/potluckfood">
-        <PotluckFood/>
+      <Route path="/potluckinvites">
+        <PotluckInvites/>
       </Route>
       <Route path='/createpotluck'>
         <CreatePotluck/>
